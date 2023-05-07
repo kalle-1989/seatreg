4012141c54e987ae11cf5d6e1fdbe6fdf1c095e4
kalle-1989 <kalle-1989@hotmail.de>
2023-05-07 17:25:24 +0200

    Set iFrame ID and height attribute instead of style height to be able to resize iFrame

--- php/seatreg_shortcode.php
+++ php/seatreg_shortcode.php
@@ -7,6 +7,6 @@
         ), $atts );
         $site_url = get_site_url();
         
-        return "<iframe style='width:100%;height:". (int)$shortcodeAtts['height'] . 'px' ."' src='". $site_url ."/?seatreg=registration&c=". $shortcodeAtts['code'] ."'></iframe>";
+        return "<iframe id='seatregFrameID' height='". (int)$shortcodeAtts['height'] . 'px' ."' style='width:100%' src='". $site_url ."/?seatreg=registration&c=". $shortcodeAtts['code'] ."'></iframe>";
     }
     add_shortcode( 'seatreg', 'seatreg_shortcode' );
